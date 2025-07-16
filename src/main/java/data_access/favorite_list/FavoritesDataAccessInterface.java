68963e011772ed0a4b2ccb2539260f3bfc3f21e6
Golbarg-max <golbarg1384@gmail.com>
2025-07-16 16:18:01 -0400

    implement data access class

--- src/main/java/data_access/favorite_list/FavoritesDataAccessInterface.java
+++ src/main/java/data_access/favorite_list/FavoritesDataAccessInterface.java
@@ -3,8 +3,8 @@ package data_access.favorite_list;
 import java.util.List;
 
 public interface FavoritesDataAccessInterface {
-    void addToFavorites(String username, String restaurantId);
-    void removeFromFavorites(String username, String restaurantId);
+    void addToFavorites(String username, String restaurantName);
+    void removeFromFavorites(String username, String restaurantName);
     List<String> getFavorites(String username);
     boolean isFavorite(String username, String restaurantId);
 }
