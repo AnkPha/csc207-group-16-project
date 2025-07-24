0f5dce2033befa950db4658235b7af159c92c56b
Golbarg-max <golbarg1384@gmail.com>
2025-07-24 17:26:22 -0400

    favorites list files

--- src/main/java/use_case/favorite_list/AddToFavoritesInteractor.java
+++ src/main/java/use_case/favorite_list/AddToFavoritesInteractor.java
@@ -2,7 +2,7 @@ package use_case.favorite_list;
 
 import data_access.favorite_list.FavoritesDataAccessInterface;
 
-public class AddToFavoritesInteractor implements AddToFavoritesInputBoundary{
+public class AddToFavoritesInteractor implements AddToFavoritesInputBoundary {
     private final FavoritesDataAccessInterface dataAccess;
 
     public AddToFavoritesInteractor(FavoritesDataAccessInterface dataAccess) {
