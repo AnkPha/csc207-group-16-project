2a1b9928fe45cfb13cf1f030314689494a86f6dc
Golbarg-max <golbarg1384@gmail.com>
2025-08-07 18:27:28 -0400

    favorite

--- src/main/java/data_access/favorite_list/FavoritesDataAccessObject.java
+++ src/main/java/data_access/favorite_list/FavoritesDataAccessObject.java
@@ -6,25 +6,27 @@ public class FavoritesDataAccessObject implements FavoritesDataAccessInterface {
     private Map<String, Set<String>> userFavorites = new HashMap<>();
 
     @Override
-    public void addToFavorites(String username, String restaurantName) {
-        userFavorites.computeIfAbsent(username, name -> new HashSet<>()).add(restaurantName);
+    public void addToFavorites(String username, String restaurantId) {
+        userFavorites.computeIfAbsent(username, name -> new HashSet<>()).add(restaurantId);
     }
 
     @Override
-    public void removeFromFavorites(String username, String restaurantName) {
+    public void removeFromFavorites(String username, String restaurantId) {
         final Set<String> favorites = userFavorites.get(username);
         if (favorites != null) {
-            favorites.remove(restaurantName);
+            favorites.remove(restaurantId);
         }
     }
 
     @Override
     public List<String> getFavorites(String username) {
-        return new ArrayList<>(userFavorites.get(username));
+        Set<String> favorites = userFavorites.get(username);
+        return favorites != null ? new ArrayList<>(favorites) : new ArrayList<>();
     }
 
     @Override
-    public boolean isFavorite(String username, String restaurantName) {
-        return userFavorites.get(username).contains(restaurantName);
+    public boolean isFavorite(String username, String restaurantId) {
+        Set<String> favorites = userFavorites.get(username);
+        return favorites != null && favorites.contains(restaurantId);
     }
 }
